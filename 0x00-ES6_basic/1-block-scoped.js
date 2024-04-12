e3d1c795885d56c96093379d11043fba8ba38df7
benaichalae <benaichalae@gmail.com>
2024-04-12 02:21:37 -0700

    es6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,14 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
-	// Changed var to const
+  // Changed var to const
   const task2 = true;
-	// Changed var to const
+  // Changed var to const
 
   if (trueOrFalse) {
     const task = true;
-	  // Changed var to const
+    // Changed var to const
     const task2 = false;
-	  // Changed var to const
+    // Changed var to const
   }
 
   return [task, task2];
