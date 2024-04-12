9856e6c53ece2165912834139f36159af6d33f4a
benaichalae <benaichalae@gmail.com>
2024-04-12 05:05:41 -0700

    es6

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve(); // Resolving the promise immediately without any data
+  });
+}
+
+export default getResponseFromAPI;
