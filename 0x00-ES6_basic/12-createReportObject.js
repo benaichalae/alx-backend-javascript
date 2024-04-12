07435176f279e8227153502a39ec70864d451413
benaichalae <benaichalae@gmail.com>
2024-04-12 02:49:25 -0700

    es6

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,8 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: { employeesList } };
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments: function (employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
 }
