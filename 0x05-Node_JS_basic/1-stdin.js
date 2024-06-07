5c8b634b48907e75e54fa400e023872b49a7da54
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2024-06-07 10:49:31 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,22 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// 1-stdin.js
+const readline = require('readline');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-
-  if (name === '') {
-    process.exit();
-  }
-
-  process.stdout.write(`Your name is: ${name}\n`);
+// Create an interface for reading data from the standard input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+// Display the welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Read the user's input
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+});
+
+// Display the closing message when the interface is closed
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
