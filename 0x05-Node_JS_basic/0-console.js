8536305a0a29ff89b6cc0b36d3ea1652813ecb76
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2024-06-07 10:21:08 +0000

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message in the console.
  * @param {string} message - The message to be displayed.
  */
-const  displayMessage = (message) => {
-  console.log(message);
-};
+function displayMessage(message) {
+    console.log(message);
+  }
 
 module.exports = displayMessage;
